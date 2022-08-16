7b314d5f29cbe78d0464ef242fe6e72db794525f
Michael Legleux <mlegleux@ripple.com>
2022-08-16 16:21:02 -0700

    echo sign whoops

--- .github/actions/sign/sign.sh
+++ .github/actions/sign/sign.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 sign_dpkg() {
   echo "GOT GPG_KEYID: ${GPG_KEYID} "
-  echo "GOT GPG_PASSPHRASE: ${GPG_PASSPHRASE}"
+  echo "GOT GPG_PASSPHRASE: $ {GPG_PASSPHRASE}"
   if [ -n "${GPG_KEYID}" ]; then
     dpkg-sig \
       -g "--no-tty --digest-algo 'sha512' --passphrase '${GPG_PASSPHRASE}' --pinentry-mode=loopback" \
